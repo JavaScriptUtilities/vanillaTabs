fec131eaf0acc906e409337797fa3031079af9b0
Kevin <kevin@rocher-dev.com>
2019-09-29 19:55:18 +0200

    v 0.3.0
    * Remove dkVanillaJS dependency.
    * Add basic demo.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Vanilla Tabs
+
+Basic JS Tabs
+
+## How to install
+
+First, load js/vanilla-tabs.js in your webpage.
+
+Then, when DOM is ready, start the plugin :
+
+```
+<script>
+new vanillaTabs({
+    triggers: document.querySelectorAll('.dk-jsu-triggers a'),
+    targets:  document.querySelectorAll('.dk-jsu-targets p')
+});
+</script>
+```
--- js/vanilla-tabs.js
+++ js/vanilla-tabs.js
@@ -1,19 +1,20 @@
 /*
  * Plugin Name: Vanilla Tabs
- * Version: 0.2
- * Plugin URL: https://github.com/Darklg/JavaScriptUtilities
- * JavaScriptUtilities Tabs may be freely distributed under the MIT license.
- * Required: Vanilla Events, Vanilla Elements, Vanilla Classes
+ * Version: 0.3.0
+ * Plugin URL: https://github.com/JavaScriptUtilities/vanillaTabs
+ * JavaScriptUtilities Vanilla Tabs may be freely distributed under the MIT license.
  */
 
 var vanillaTabs = function(settings) {
     var self = this,
         triggers = false,
         targets = false;
+
     self.defaultSettings = {
         classCurrent: 'is-current',
         callBackTab: function(i) {}
     };
+
     self.init = function(settings) {
         self.getSettings(settings);
         // Check if triggers and targets exists
@@ -23,54 +24,63 @@ var vanillaTabs = function(settings) {
         triggers = self.settings.triggers;
         targets = self.settings.targets;
         // Prevent double launch
-        if (!triggers[0] || triggers[0].hasClass('has-vanilla-tabs')) {
+        if (!triggers[0] || triggers[0].getAttribute('data-vanilla-tabs')) {
             return;
         }
-        triggers[0].addClass('has-vanilla-tabs');
+
         // Set Elements & events
         self.setElements();
         self.setEvents();
+
         // Go to tab #1
         self.setTab(0);
     };
+
     self.setElements = function() {
         var i = 0;
-        triggers.eachElement(function(el) {
+        triggers.forEach(function(el) {
+            el.setAttribute('data-vanilla-tabs', '1');
             el.setAttribute('data-i', i++);
         });
     };
+
     self.setEvents = function() {
-        triggers.eachElement(function(el) {
-            el.addEvent('click', function(e) {
-                window.eventPreventDefault(e);
+        triggers.forEach(function(el) {
+            el.addEventListener('click', function(e) {
+                e.preventDefault();
                 // Get trigger number
                 var i = parseInt(el.getAttribute('data-i'), 10);
                 // Set called tab
                 self.setTab(i);
-            });
+            }, 1);
         });
     };
+
     self.setTab = function(i) {
         var classCurrent = self.settings.classCurrent,
             callBackTab = self.settings.callBackTab;
         if (!triggers[i] || !targets[i]) {
             return;
         }
+
         // Remove class on all triggers and targets
-        triggers.eachElement(function(el) {
-            el.removeClass(classCurrent);
+        triggers.forEach(function(el) {
+            el.classList.remove(classCurrent);
         });
-        targets.eachElement(function(el) {
-            el.removeClass(classCurrent);
+        targets.forEach(function(el) {
+            el.classList.remove(classCurrent);
         });
+
         // Add class on current trigger & target
-        triggers[i].addClass(classCurrent);
-        targets[i].addClass(classCurrent);
+        triggers[i].classList.add(classCurrent);
+        targets[i].classList.add(classCurrent);
+
         // Launch callback
         if (typeof callBackTab == 'function') {
             callBackTab(i);
         }
     };
+
     self.init(settings);
     return self;
 };
