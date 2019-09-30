4eab29c695ace8303bacb1302b8743e9f1ef6840
Kevin <kevin@rocher-dev.com>
2019-09-30 22:18:23 +0200

    v 0.4.0
    * Add minified version.
    * Add basic aria compatibility.

---
+++ grunt/uglify.js
@@ -0,0 +1,7 @@
+module.exports = {
+    all: {
+        files: {
+            'js/vanilla-tabs.min.js': ['js/vanilla-tabs.js']
+        }
+    }
+};
--- js/vanilla-tabs.js
+++ js/vanilla-tabs.js
@@ -1,6 +1,6 @@
 /*
  * Plugin Name: Vanilla Tabs
- * Version: 0.3.0
+ * Version: 0.4.0
  * Plugin URL: https://github.com/JavaScriptUtilities/vanillaTabs
  * JavaScriptUtilities Vanilla Tabs may be freely distributed under the MIT license.
  */
@@ -10,70 +10,70 @@ var vanillaTabs = function(settings) {
         triggers = false,
         targets = false;
 
-    self.defaultSettings = {
+    /* Settings */
+    var _settings = {
         classCurrent: 'is-current',
+        baseAttr: 'data-vanilla-tabs',
         callBackTab: function(i) {}
     };
 
+    // Set new values
+    for (var attr2 in settings) {
+        _settings[attr2] = settings[attr2];
+    }
+
     self.init = function(settings) {
-        self.getSettings(settings);
         // Check if triggers and targets exists
-        if (!self.settings.triggers || !self.settings.targets) {
+        if (!_settings.triggers || !_settings.targets) {
             return false;
         }
-        triggers = self.settings.triggers;
-        targets = self.settings.targets;
+        triggers = _settings.triggers;
+        targets = _settings.targets;
+
         // Prevent double launch
-        if (!triggers[0] || triggers[0].getAttribute('data-vanilla-tabs')) {
+        if (!triggers[0] || triggers[0].getAttribute(_settings.baseAttr)) {
             return;
         }
 
-        // Set Elements & events
-        self.setElements();
-        self.setEvents();
+        // Set triggers
+        (function() {
+            var i = 0;
+            triggers.forEach(function(el) {
+                el.setAttribute(_settings.baseAttr, '1');
+                el.setAttribute('data-i', i++);
+                el.addEventListener('click', function(e) {
+                    e.preventDefault();
+                    // Get trigger number
+                    var i = parseInt(el.getAttribute('data-i'), 10);
+                    // Set called tab
+                    self.setTab(i);
+                }, 1);
+            });
+        }());
 
         // Go to tab #1
         self.setTab(0);
     };
 
-    self.setElements = function() {
-        var i = 0;
-        triggers.forEach(function(el) {
-            el.setAttribute('data-vanilla-tabs', '1');
-            el.setAttribute('data-i', i++);
-        });
-    };
-
-    self.setEvents = function() {
-        triggers.forEach(function(el) {
-            el.addEventListener('click', function(e) {
-                e.preventDefault();
-                // Get trigger number
-                var i = parseInt(el.getAttribute('data-i'), 10);
-                // Set called tab
-                self.setTab(i);
-            }, 1);
-        });
-    };
-
     self.setTab = function(i) {
-        var classCurrent = self.settings.classCurrent,
-            callBackTab = self.settings.callBackTab;
+        var classCurrent = _settings.classCurrent,
+            callBackTab = _settings.callBackTab;
+
         if (!triggers[i] || !targets[i]) {
             return;
         }
 
         // Remove class on all triggers and targets
-        triggers.forEach(function(el) {
-            el.classList.remove(classCurrent);
-        });
-        targets.forEach(function(el) {
-            el.classList.remove(classCurrent);
+        triggers.forEach(function(el, iEl) {
+            el.classList[iEl == i ? 'add' : 'remove'](classCurrent);
+            el.setAttribute('aria-selected', iEl == i ? 'true' : 'false');
         });
 
-        // Add class on current trigger & target
-        triggers[i].classList.add(classCurrent);
-        targets[i].classList.add(classCurrent);
+        // Set active
+        targets.forEach(function(el, iEl) {
+            el.classList[iEl == i ? 'add' : 'remove'](classCurrent);
+            el.setAttribute('aria-hidden', iEl == i ? 'false' : 'true');
+        });
 
         // Launch callback
         if (typeof callBackTab == 'function') {
@@ -83,20 +83,4 @@ var vanillaTabs = function(settings) {
 
     self.init(settings);
     return self;
-};
-
-/* Get Settings */
-vanillaTabs.prototype.getSettings = function(settings) {
-    if (typeof settings != 'object') {
-        settings = {};
-    }
-    this.settings = {};
-    // Set default values
-    for (var attr in this.defaultSettings) {
-        this.settings[attr] = this.defaultSettings[attr];
-    }
-    // Set new values
-    for (var attr2 in settings) {
-        this.settings[attr2] = settings[attr2];
-    }
 };
